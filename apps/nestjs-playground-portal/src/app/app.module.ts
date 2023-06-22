a05a77150c6a6e60ccb12124ee4a3d7d9ccff2d0
Aymen Hachicha <hachicha@infosim.net>
2023-06-22 09:02:57 +0200

    logins works with auth0

--- apps/nestjs-playground-portal/src/app/app.module.ts
+++ apps/nestjs-playground-portal/src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { AuthModule } from '@auth0/auth0-angular';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { environment } from './environments/environment';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, LoginPageComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AuthModule.forRoot({
+      domain: environment.auth.domain,
+      clientId: environment.auth.clientId,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+    RouterModule.forRoot(appRoutes),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- apps/nestjs-playground-portal/src/app/pages/login-page/login-page.component.ts
+++ apps/nestjs-playground-portal/src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'ah-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: [],
 })
-export class LoginPageComponent {
-  title = 'login-page';
-}
+export class LoginPageComponent {}
